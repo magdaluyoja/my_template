20c464df69fccf6abca9d584fedb7fe312b25834
Jay-R Magdaluyo <j.magdaluyo@seiko-it.com.ph.local>
2018-08-22 08:09:48 +0800

    Working JS AMD Sample Page 2

--- public/admin/js/pages/sample.js
+++ public/admin/js/pages/sample.js
@@ -1,6 +1,1 @@
-window.$ = window.jQuery = require("jquery");
-require('bootstrap-sass');
-require("jquery-slimscroll");
-require("fastclick");
-require("../template.js");
-require("../demo.js");
+require("../common.js");
